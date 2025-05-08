596a1abd42e69b315f8a8a10e4ed6d84186c5fdd
Yannick Van Heerden <134289088+yannickvhh@users.noreply.github.com>
2025-05-08 22:47:12 +0200

    Update main.cpp
    add encodeur code and go straight 20cm test

--- main.cpp
+++ main.cpp
@@ -25,94 +25,90 @@
 #define ADDR_IMU 0x6B
 #define ADDR_MAG 0x1E
 
-    //Test Led CLignotement:
-    //Rajout test Moteur droit:
-void setup(){
-    //config broches
-    pinMode(LEDU1, OUTPUT);
 
-    pinMode(EN_D, OUTPUT);//moteur droit
-    pinMode(IN_1_D, OUTPUT);//def droit arriere
-    pinMode(IN_2_D, OUTPUT);//def droit avant
-    
-    pinMode(EN_G, OUTPUT);// moteur gauche
-    pinMode(IN_1_G, OUTPUT);//def gauche avant
-    pinMode(IN_2_G, OUTPUT);//def gauche arriere
+volatile int compteurG = 0; // Compteur pour la roue gauche / volatile parceque les variables peuvent changer 
+volatile int compteurD = 0; // Compteur pour la roue droite
 
-    //activation moteur :
-    digitalWrite(EN_D, HIGH);//envoi courant au moteur droit
-    digitalWrite(EN_G,HIGH);//envoi courant moteur gauche
+const float circonferenceRoue = 28.27; // en cm, pour roue de 9 cm de diamètre
+const int topsParTour = 1060; // nombre de tops par tour de roue (1060 pour encodeur quadrature)
+int objectfif = 20; // Objectif de distance à parcourir (en cm)
+bool enMarche = false; // Etat du robot (en marche ou pas)
+
+// Interruption pour roue gauche
+void IRAM_ATTR interruptionGauche() {
+  if(digitalRead(ENC_G_CH_B)==HIGH){//iram_attr appele par le microcontroleur quand il detecte un top sur une des broches
+  compteurG--;// decrementer le compteur gauche
+}else{
+  compteurG++;// incrementer le compteur gauche
+}
 }
 
-void loop(){
-    //test clignotement led 2fois:
-    digitalWrite(LEDU1, HIGH);
-    delay(500);
-    digitalWrite(LEDU1, LOW);
-    delay(500);
-    digitalWrite(LEDU1, HIGH);
-    delay(500);
-    digitalWrite(LEDU1, LOW);
-    delay(500);
+// Interruption pour roue droite
+void IRAM_ATTR interruptionDroite() {
+  if(digitalRead(ENC_D_CH_B)==HIGH){//iram_attr appele par le microcontroleur quand il detecte un top sur une des broches
+  compteurD++;// incrementer le compteur droit
+}else{
+  compteurD--;// decrementer le compteur droit
+}
+}
 
 
-    //test tourner roue droite:
-    digitalWrite(IN_1_D, LOW);//active la roue DROITE en avant pdt 1sec
-    digitalWrite(IN_2_D, HIGH);
-    delay(1000);
 
-    digitalWrite(IN_1_D, LOW);//arret pdt 1sec
-    digitalWrite(IN_2_D, LOW);
-    delay(1000);
+void setup() {
+  Serial.begin(115200);// Initialiser la communication série à 115200 bauds
 
-    digitalWrite(IN_1_D, HIGH);//active la roue DROITE en arriere pdt 1sec
-    digitalWrite(IN_2_D, LOW);
-    delay(1000);
+  //config broches
+  pinMode(LEDU1, OUTPUT);
+
+  pinMode(EN_D, OUTPUT);//moteur droit
+  pinMode(IN_1_D, OUTPUT);//def droit arriere
+  pinMode(IN_2_D, OUTPUT);//def droit avant
     
-    digitalWrite(IN_1_D,LOW);//arret 2sec
-    digitalWrite(IN_2_D,LOW);
-    delay(2000);
+  pinMode(EN_G, OUTPUT);// moteur gauche
+  pinMode(IN_1_G, OUTPUT);//def gauche avant
+  pinMode(IN_2_G, OUTPUT);//def gauche arriere
 
+  //activation MOTEUR :
+  digitalWrite(EN_D, HIGH);//envoi courant au moteur droit
+  digitalWrite(EN_G,HIGH);//envoi courant moteur gauche
 
-    //test tourner roue GAUCHE:
-    digitalWrite(IN_1_G, HIGH);//active la roue GAUCHE en avant pdt 1sec
-    digitalWrite(IN_2_G, LOW);
-    delay(1000);
+  // Broches en entrée
+  pinMode(ENC_G_CH_A, INPUT);// Configurer la broche de l'encodeur gauche comme entrée
+  pinMode(ENC_D_CH_A, INPUT);// Configurer la broche de l'encodeur droit comme entrée
+  pinMode(ENC_G_CH_B, INPUT);// Configurer la broche de l'encodeur gauche comme entrée
+  pinMode(ENC_D_CH_B, INPUT);// Configurer la broche de l'encodeur droit comme entrée
 
-    digitalWrite(IN_1_G, LOW);//arret pdt 1sec
-    digitalWrite(IN_2_G, LOW);
-    delay(1000);
+  // Attacher les interruptions aux deux encodeurs (front montant)
+  attachInterrupt(digitalPinToInterrupt(ENC_G_CH_A), interruptionGauche, RISING);//des que tension monte a 1 sur lune des broche 32 ou 27 on appelle la fnc intterruptionGauche
+  attachInterrupt(digitalPinToInterrupt(ENC_D_CH_A), interruptionDroite, RISING);
 
-    digitalWrite(IN_1_G, LOW);//active la roue GAUCHE en arriere pdt 1sec
-    digitalWrite(IN_2_G, HIGH);
-    delay(1000);
-    
-    digitalWrite(IN_1_G,LOW);//ARRET 2 sec
-    digitalWrite(IN_2_G,LOW);
-    delay(2000);
+  //calcul nbr de tops pr 20cm
+  objectfif=(20.0/ circonferenceRoue)* topsParTour; // 20 cm de distance à parcourir
 
-    //TEST AVANCER pdt 1sec
-    digitalWrite(IN_1_D, LOW);//active la roue DROITE en avant
-    digitalWrite(IN_2_D, HIGH);
-    digitalWrite(IN_1_G,HIGH);//active la roue GAUCHE en avant
-    digitalWrite(IN_2_G,LOW);
-    delay(1000);
-    //TEST ARRET PDT 2SEC
-    digitalWrite(IN_1_D, LOW);//ARRETE la roue DROITE
-    digitalWrite(IN_2_D, LOW);
-    digitalWrite(IN_1_G,LOW);//ARRETE la roue GAUCHE 
-    digitalWrite(IN_2_G,LOW);
-    delay(2000);
-    //TEST RECULER pdt 1sec
-    digitalWrite(IN_1_D, HIGH);
-    digitalWrite(IN_2_D, LOW);//ACTIVE la roue DROITE en arriere
-    digitalWrite(IN_1_G,LOW);
-    digitalWrite(IN_2_G,HIGH);//ACTIVE la roue GAUCHE en arriere
-    delay(1000);
-//TEST ARRET PDT 10SEC
-    digitalWrite(IN_1_D, LOW);
-    digitalWrite(IN_2_D, LOW);//ARRETE la roue DROITE 
-    digitalWrite(IN_1_G,LOW);
-    digitalWrite(IN_2_G,LOW);//ARRETE la roue GAUCHE 
-    delay(10000);
+  digitalWrite(IN_1_D , LOW); // Moteur droit arriere
+  digitalWrite(IN_2_D , HIGH); // Moteur droit avant   
+  digitalWrite(IN_1_G , HIGH); // Moteur gauche avant
+  digitalWrite(IN_2_G , LOW); // Moteur gauche arriere  
+  enMarche  = true; // Le robot est en marche
+  compteurD=0; // Initialiser le compteur droit à 0
+  compteurG=0; // Initialiser le compteur gauche à 0
+}
+
+void loop() {
+  if(enMarche && abs(compteurG) >= objectfif && abs(compteurD) >= objectfif) { // Si le robot a parcouru la distance souhaitée
+    digitalWrite(IN_1_D , LOW); // Moteur droit arriere
+    digitalWrite(IN_2_D , LOW); // Moteur droit avant   
+    digitalWrite(IN_1_G , LOW); // Moteur gauche avant
+    digitalWrite(IN_2_G , LOW); // Moteur gauche arriere  
+    Serial.println("Objectif atteint ! Distance de 20cm atteinte !"); // Afficher un message sur le moniteur série
+    enMarche = false; // Le robot s'arrête
+  }
+  float distanceG = (compteurG* circonferenceRoue) /topsParTour; // Calculer la distance parcourue par la roue gauche
+  float distanceD = (compteurD * circonferenceRoue) / topsParTour; // Calculer la distance parcourue par la roue droite
+  Serial.print("distance Gauche : ");
+  Serial.print(distanceG); // Afficher la distance gauche
+  Serial.print("cm | distance Droite : ");
+  Serial.println(distanceD);//afficher la distance droite
+  Serial.println(" cm ");
+  delay(500); // Affiche toutes les 0.5 sec
 }
